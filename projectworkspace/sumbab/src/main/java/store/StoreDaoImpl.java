8358a024d1c1454a003d81241e07b8f01e9a00a8
JudithKim115 <82377269+JudithKim115@users.noreply.github.com>
2021-04-22 22:42:23 +0900

    Update StoreDaoImpl.java

--- projectworkspace/sumbab/src/main/java/store/StoreDaoImpl.java
+++ projectworkspace/sumbab/src/main/java/store/StoreDaoImpl.java
@@ -12,17 +12,14 @@ import store.restCat.RestCat;
 public class StoreDaoImpl implements StoreDao {
 	
 	@Autowired
-	private SqlSessionTemplate sqlSessionTemplate; //이거 선언이 없네요 DB연동 빈이 등록되어 있지않습니다.
+	private SqlSessionTemplate sqlSessionTemplate;
 
-	//Component-scan 
-	//@Component, @Controller, @Service, @Repository
-	//@Autowired, @Resource (X사용할일 거의 없습니다), @Inject 
-	
 	@Autowired
 	public StoreDaoImpl(SqlSessionTemplate sqlSessionTemplate) {
 		this.sqlSessionTemplate = sqlSessionTemplate;
 	}
 	
+	//=========기본 crud 기능================
 	@Override
 	public void insert(Store store) {
 		sqlSessionTemplate.insert("insert", store);
@@ -34,45 +31,27 @@ public class StoreDaoImpl implements StoreDao {
 	}
 
 	@Override
+	public void delete(Store store) {
+		sqlSessionTemplate.delete("delete",store);
+	}
+	
+	//========검색 관련 기능 ====================
+	@Override //일반검색
 	public List<Store> selectByKeyword(String keyword) {
 		List<Store> results = sqlSessionTemplate.selectList("selectByKeyword", keyword);
 		return results;
 	}
 
-	@Override
+	@Override //특정 해시태그 가진 식당 검색
 	public List<Store> selectByTag(String tag) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Store> results = sqlSessionTemplate.selectList("selectByTag", tag);
+		return results;
 	}
 
-	@Override
-	public List<Store> selectNearby(Store store) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Store> selectByEditor(int editorNum) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void delete(Store store) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public List<Store> selectByRestCat(RestCat restCat) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Store selectByName(String storeName) {
-		List<Store> result = sqlSessionTemplate.selectOne("selectByName", storeName);
-		return result.isEmpty() ? null : result.get(0);
+	@Override //숨밥 시작하기 : 카테고리 검색-식당
+	public List<Store> selectByRestCat(String category) {
+		List<Store> results = sqlSessionTemplate.selectList("selectByRestCat", category);
+		return results;
 	}
 
 }
