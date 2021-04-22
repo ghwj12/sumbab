d1f8bf80835a0afb899c2132f128bb83f91423c6
JudithKim115 <82377269+JudithKim115@users.noreply.github.com>
2021-04-23 01:21:26 +0900

    Update StoreController.java

--- projectworkspace/sumbab/src/main/java/store/StoreController.java
+++ projectworkspace/sumbab/src/main/java/store/StoreController.java
@@ -1,14 +1,73 @@
 package store;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import store.cafeCat.CafeCatRegistReq;
+import store.cafeCat.CafeCatRegitService;
+import store.restCat.RestCatRegistReq;
+import store.restCat.RestCatRegitService;
+
+@Component
 @Controller
 public class StoreController {
 
-	@RequestMapping(value="/registerStore")
-	public String regitStore() {
-		return "/store/registerStore";
+	@RequestMapping(value = "/store/registerStep1", method = RequestMethod.GET)
+	public String regitStore1() {
+		return "store/registerStep1";
 	}
+
+	@Autowired
+	private StoreRegisterService storeRegisterService;
+
+	@Autowired
+	public void setStoreRegisterService(StoreRegisterService storeRegisterService) {
+		this.storeRegisterService = storeRegisterService;
+	}
+
+	@RequestMapping(value = "/store/registerStep1-2", method = RequestMethod.POST) // step1에서 submit->register2로 보냄
+	public String regitStore2(StoreRegisterRequest regReq) { // regitrequest 요청
+		storeRegisterService.regist(regReq);
+		if(regReq.getClassify().equals("음식점")) {
+			return "store/registerStep2_res";
+		}else {
+			return "store/registerStep2_cafe";
+		}
+	}
+
+	@RequestMapping(value = "/store/registerStep1-2", method = RequestMethod.GET) // get으로 접근하면 regit1로 돌려보내기
+	public String regitStore2() {
+
+		return "redirect:/store/registerStep1";
+	}
+
+	@Autowired
+	private RestCatRegitService restCatRegitService;
 	
+	@Autowired
+	public void setRestCatRegitService(RestCatRegitService restCatRegitService) {
+		this.restCatRegitService = restCatRegitService;
+	}
+	@Autowired
+	private CafeCatRegitService cafeCatRegitService;
+	
+	@Autowired
+	public void setCafeCatRegitService(CafeCatRegitService cafeCatRegitService) {
+		this.cafeCatRegitService = cafeCatRegitService;
+	}
+	@RequestMapping(value = "/store/completeRegisterR", method = RequestMethod.POST) // 카테고리 작성 후 마지막 complete 페이지 요청
+	public String regitComplete(RestCatRegistReq resCatRegitReq) {
+		
+		restCatRegitService.resgist(resCatRegitReq);
+		return "store/completeRegister";
+	}
+	@RequestMapping(value = "/store/completeRegisterC", method = RequestMethod.POST) // 카테고리 작성 후 마지막 complete 페이지 요청
+	public String regitComplete(CafeCatRegistReq cafeCatRegitReq) {
+		
+		cafeCatRegitService.resgist(cafeCatRegitReq);
+		return "store/completeRegister";
+	}
 }
