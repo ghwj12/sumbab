85c229269aecbd3d8d273690bf31f6d87af7a694
JudithKim115 <82377269+JudithKim115@users.noreply.github.com>
2021-04-26 03:09:03 +0900

    Update StoreController.java

--- projectworkspace/sumbab/src/main/java/store/StoreController.java
+++ projectworkspace/sumbab/src/main/java/store/StoreController.java
@@ -1,25 +1,28 @@
 package store;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import store.cafeCat.CafeCatRegistReq;
 import store.cafeCat.CafeCatRegitService;
 import store.restCat.RestCatRegistReq;
 import store.restCat.RestCatRegitService;
 
-@Component
 @Controller
+@RequestMapping("/store/*")
+@SessionAttributes("storeVO")
 public class StoreController {
 
-	@RequestMapping(value = "/store/registerStep1", method = RequestMethod.GET)
-	public String regitStore1() {
+	@RequestMapping(value = "/registerStep1", method = RequestMethod.GET)
+	public String regitStore1(Model model) {
+		model.addAttribute("storeVO", new Store());
 		return "store/registerStep1";
 	}
-
 	@Autowired
 	private StoreRegisterService storeRegisterService;
 
@@ -28,9 +31,11 @@ public class StoreController {
 		this.storeRegisterService = storeRegisterService;
 	}
 
-	@RequestMapping(value = "/store/registerStep1-2", method = RequestMethod.POST) // step1에서 submit->register2로 보냄
-	public String regitStore2(StoreRegisterRequest regReq) { // regitrequest 요청
+	@RequestMapping(value = "/registerStep1-2", method = RequestMethod.POST) 
+	public String regitStore2(@ModelAttribute("storeVO")Store store, StoreRegisterRequest regReq) {
+		
 		storeRegisterService.regist(regReq);
+		
 		if(regReq.getClassify().equals("음식점")) {
 			return "store/registerStep2_res";
 		}else {
@@ -38,7 +43,7 @@ public class StoreController {
 		}
 	}
 
-	@RequestMapping(value = "/store/registerStep1-2", method = RequestMethod.GET) // get으로 접근하면 regit1로 돌려보내기
+	@RequestMapping(value = "/registerStep1-2", method = RequestMethod.GET) // get으로 접근하면 regit1로 돌려보내기
 	public String regitStore2() {
 
 		return "redirect:/store/registerStep1";
@@ -58,13 +63,12 @@ public class StoreController {
 	public void setCafeCatRegitService(CafeCatRegitService cafeCatRegitService) {
 		this.cafeCatRegitService = cafeCatRegitService;
 	}
-	@RequestMapping(value = "/store/completeRegisterR", method = RequestMethod.POST) // 카테고리 작성 후 마지막 complete 페이지 요청
+	@RequestMapping(value = "/completeRegisterR", method = RequestMethod.POST) // 카테고리 작성 후 마지막 complete 페이지 요청
 	public String regitComplete(RestCatRegistReq resCatRegitReq) {
-		
 		restCatRegitService.resgist(resCatRegitReq);
 		return "store/completeRegister";
 	}
-	@RequestMapping(value = "/store/completeRegisterC", method = RequestMethod.POST) // 카테고리 작성 후 마지막 complete 페이지 요청
+	@RequestMapping(value = "/completeRegisterC", method = RequestMethod.POST) // 카테고리 작성 후 마지막 complete 페이지 요청
 	public String regitComplete(CafeCatRegistReq cafeCatRegitReq) {
 		
 		cafeCatRegitService.resgist(cafeCatRegitReq);
