3f56ce8563dd944df2c9265f7c0a56b40719ada4
JudithKim115 <82377269+JudithKim115@users.noreply.github.com>
2021-04-25 18:39:24 +0900

    Create popup.js

---
+++ projectworkspace/sumbab/src/main/webapp/resources/js/popup.js
@@ -0,0 +1,8 @@
+function popup() {
+	var url = "addPhoto";
+	var name = "사진 첨부하기";
+
+	var option = "width = 500, height = 500, toolbar=no,location=no, status=no, scrollbars=no"
+		
+	window.open(url, name, option);
+}
