8f95021f2e0b923beb4605dffd68d214483081b7
JudithKim115 <82377269+JudithKim115@users.noreply.github.com>
2021-04-28 16:05:41 +0900

    Create Reserve.java

---
+++ projectworkspace/sumbab/src/main/java/my/reserve/Reserve.java
@@ -0,0 +1,60 @@
+package my.reserve;
+
+public class Reserve {
+
+	private int storeNum;
+	private String id;
+	private String reserveName;
+	private String resDate;
+	private String phone;
+	
+	public Reserve() {}
+	
+	public Reserve(String reserveName, String resDate, String phone) {
+		super();
+		this.reserveName = reserveName;
+		this.resDate = resDate;
+		this.phone = phone;
+	}
+
+	public int getStoreNum() {
+		return storeNum;
+	}
+
+	public void setStoreNum(int storeNum) {
+		this.storeNum = storeNum;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getReserveName() {
+		return reserveName;
+	}
+
+	public void setReserveName(String reserveName) {
+		this.reserveName = reserveName;
+	}
+
+	public String getResDate() {
+		return resDate;
+	}
+
+	public void setResDate(String resDate) {
+		this.resDate = resDate;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+}
