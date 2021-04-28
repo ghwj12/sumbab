73f5e10bff7c38a17e9596b36acf63e9581969ef
JudithKim115 <82377269+JudithKim115@users.noreply.github.com>
2021-04-29 01:30:48 +0900

    Update ReserveDao.java

--- projectworkspace/sumbab/src/main/java/my/reserve/ReserveDao.java
+++ projectworkspace/sumbab/src/main/java/my/reserve/ReserveDao.java
@@ -3,10 +3,15 @@ package my.reserve;
 import java.sql.Date;
 import java.util.List;
 
+import store.Store;
+
 public interface ReserveDao {
 
 	public void insert(Reserve reserve);//예약하기
 	public void delete(Date sysdate); //예약 삭제(예약시간 1시간 전까지만. 별도 수정 없이 취소만 가능)
 	
-	public List<Reserve> list(String memberId); //memberVO.id로 예약리스트 가져오기
+	public List<Reserve> listFuture(String memberID); //memberVO.id로 예약리스트 가져오기
+	public List<Reserve> listPast(String memberID); //memberVO.id로 예약리스트 가져오기
+	
+	public List<Store> getStoreName(String memberID);
 }
