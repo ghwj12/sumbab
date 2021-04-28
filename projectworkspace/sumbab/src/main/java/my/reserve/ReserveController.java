446bc199639942f6f6f797aa8a0b6865b01ea92c
JudithKim115 <82377269+JudithKim115@users.noreply.github.com>
2021-04-28 16:05:17 +0900

    Update ReserveController.java

--- projectworkspace/sumbab/src/main/java/my/reserve/ReserveController.java
+++ projectworkspace/sumbab/src/main/java/my/reserve/ReserveController.java
@@ -1,17 +1,45 @@
 package my.reserve;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
+@SessionAttributes("reserveDTO")
 public class ReserveController {
+	
+	@Autowired
+	private ReserveService reserveService;
+	
+	@Autowired
+	public void setReserveService(ReserveService reserveService) {
+		this.reserveService = reserveService;
+	}
 
-
-	@RequestMapping(value="/reserve", method=RequestMethod.GET)
+	//마이페이지-->나의 예약한 음식점 리스트 보기:리스트 출력
+	@RequestMapping(value="/reserveList", method=RequestMethod.GET)
 	public String reserveList() {
 		
 		return "reserve/reserveList";
 	}
 	
+	//식당 상세보기-->예약하기 요청
+	@RequestMapping(value="/reserve/reserveForm", method=RequestMethod.GET)//나중에 post로 변경
+	public String reserve(Model model) {
+		model.addAttribute("reserveDTO", new Reserve());
+		return "reserve/reserveForm";
+	}
+	
+	@RequestMapping(value="reserve/completeReserve", method=RequestMethod.POST)
+	public String alignValue(@ModelAttribute("reserveDTO")Reserve reserve, ReserveRegitReq resReq, String datepicker, String timepicker) {
+		reserveService.alignValue(resReq, datepicker, timepicker);
+		
+		return "reserve/completeReserve";
+	}
+	
+
 }
