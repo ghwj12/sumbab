ace5a3b306e225576efe85b67520b68e804a6434
JudithKim115 <82377269+JudithKim115@users.noreply.github.com>
2021-04-29 01:30:40 +0900

    Update ReserveService.java

--- projectworkspace/sumbab/src/main/java/my/reserve/ReserveService.java
+++ projectworkspace/sumbab/src/main/java/my/reserve/ReserveService.java
@@ -1,7 +1,11 @@
 package my.reserve;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import store.Store;
 
 @Service
 public class ReserveService {
@@ -15,17 +19,26 @@ public class ReserveService {
 		this.reserveDao = reserveDao;
 	}
 	
-	public void alignValue(ReserveRegitReq resReq, String datepicker, String timepicker) {
-		String resDate = datepicker+" "+timepicker+":00";
-		System.out.println(resDate);//확인용
+	public void regit(ReserveRegitReq resReq, String datepicker, String timepicker) {
+		String resDate = datepicker+" "+timepicker;
 		
 		Reserve newReserve = new Reserve(
 				resReq.getReserveName(),
 				resDate,
 				resReq.getPhone()
 				);
-		
 		reserveDao.insert(newReserve);
 	}
 	
+	public List<Reserve> getListF(String memberID) {
+		return reserveDao.listFuture(memberID);
+	}
+	
+	public List<Reserve> getListP(String memberID) {
+		return reserveDao.listPast(memberID);
+	}
+	
+	public List<Store> getStoreName(String memberID){
+		return reserveDao.getStoreName(memberID);
+	}
 }
