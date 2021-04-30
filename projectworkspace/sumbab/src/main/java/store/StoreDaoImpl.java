55e509f76c934aaca92cb2f2d8a6ac9f758d0fa7
JudithKim115 <82377269+JudithKim115@users.noreply.github.com>
2021-05-01 03:59:54 +0900

    Update StoreDaoImpl.java

--- projectworkspace/sumbab/src/main/java/store/StoreDaoImpl.java
+++ projectworkspace/sumbab/src/main/java/store/StoreDaoImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class StoreDaoImpl implements StoreDao {
-	
+
 	@Autowired
 	private SqlSessionTemplate sqlSessionTemplate;
 
@@ -16,43 +16,30 @@ public class StoreDaoImpl implements StoreDao {
 	public StoreDaoImpl(SqlSessionTemplate sqlSessionTemplate) {
 		this.sqlSessionTemplate = sqlSessionTemplate;
 	}
-	
-	//=========기본 CRUD 기능================
+
+	// =========기본 CRUD 기능================
 	@Override
 	public void insert(Store store) {
-		sqlSessionTemplate.insert("insert", store);
+		sqlSessionTemplate.insert("insertStore", store);
 	}
 
 	@Override
 	public void update(Store store) {
-		sqlSessionTemplate.update("update", store);
+		sqlSessionTemplate.update("updateStore", store);
 	}
 
 	@Override
 	public void delete(Store store) {
-		sqlSessionTemplate.delete("delete",store);
-	}
-	
-	//========검색 관련 기능 ====================
-	@Override //일반검색
-	public List<Store> selectByKeyword(String keyword) {
-		List<Store> results = sqlSessionTemplate.selectList("selectByKeyword", keyword);
-		return results;
+		sqlSessionTemplate.delete("deleteStore", store);
 	}
 
-	@Override //특정 해시태그 가진 식당 검색
-	public List<Store> selectByTag(String tag) {
-		List<Store> results = sqlSessionTemplate.selectList("selectByTag", tag);
-		return results;
-	}
-
-	@Override //숨밥 시작하기 : 카테고리 검색-식당
+	@Override // 숨밥 시작하기 : 카테고리 검색-식당
 	public List<Store> selectByRestCat(String category) {
 		List<Store> results = sqlSessionTemplate.selectList("selectByRestCat", category);
 		return results;
 	}
-	
-	@Override //숨밥 시작하기 : 카테고리 검색-식당
+
+	@Override // 숨밥 시작하기 : 카테고리 검색-식당
 	public List<Store> selectByCafeCat(String category) {
 		List<Store> results = sqlSessionTemplate.selectList("selectByCafeCat", category);
 		return results;
