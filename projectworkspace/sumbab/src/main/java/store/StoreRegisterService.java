fb517148a8105744aa7dd501eae637e29cfd528b
JudithKim115 <82377269+JudithKim115@users.noreply.github.com>
2021-05-01 04:00:10 +0900

    Update StoreRegisterService.java

--- projectworkspace/sumbab/src/main/java/store/StoreRegisterService.java
+++ projectworkspace/sumbab/src/main/java/store/StoreRegisterService.java
@@ -15,12 +15,21 @@ public class StoreRegisterService {
 		this.storeDao = storeDao;
 	}
 	
-	public void regist(StoreRegisterRequest req) {
-
+	public void regist(StoreRegisterRequest req, String fullAddress, String extrAddress) {
+		
+		int space = fullAddress.indexOf(" ");
+		int secspace = fullAddress.indexOf(" ",space+1);
+		
+		String cityCode = fullAddress.substring(0,space);
+		String districtCode = fullAddress.substring(space+1,secspace);
+		String address = fullAddress.substring(secspace+1)+" "+extrAddress;
+		
 		Store newStore = new Store(
 				req.getClassify(),
 				req.getName(),
-				req.getAddress(),
+				cityCode,
+				districtCode,
+				address,
 				req.getPhone(),
 				req.getMenu(),
 				req.getTime(),
