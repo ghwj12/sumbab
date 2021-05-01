ccb8e0c8500e2e9669784f1caaeab69adef9b20e
JudithKim115 <82377269+JudithKim115@users.noreply.github.com>
2021-05-02 01:23:26 +0900

    Update StoreDao.java
    가게 상세보기 관련 dao 추가

--- projectworkspace/sumbab/src/main/java/store/StoreDao.java
+++ projectworkspace/sumbab/src/main/java/store/StoreDao.java
@@ -10,6 +10,10 @@ public interface StoreDao {
 	
 	public List<Store> selectByRestCat(String category);
 	public List<Store> selectByCafeCat(String category);
+	
+	public Store selectOne(int storeNum); //가게 상세보기
+	public void upReadCount(int storeNum);//가게 조회수 증가
+	
 	//public List<Store> selectNearby(Store store); //현재위치값 들어있는 cords
 	//public List<Store> selectByEditor(int editorNum); //editor table의 가게목록 seq 받아올 것
 	
