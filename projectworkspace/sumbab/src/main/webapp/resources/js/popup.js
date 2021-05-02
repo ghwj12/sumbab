9a8a7d7aff78c2f4b23718bd6034dbc3f560373b
JudithKim115 <82377269+JudithKim115@users.noreply.github.com>
2021-05-02 20:37:17 +0900

    Update popup.js
    팝업JS 추가
    
    1. popup() : 리뷰 작성 시, 사진 첨부하는 팝업(미완성)
    2. popupForCancel : 예약 취소 안내 팝업(완료)

--- projectworkspace/sumbab/src/main/webapp/resources/js/popup.js
+++ projectworkspace/sumbab/src/main/webapp/resources/js/popup.js
@@ -1,8 +1,15 @@
 function popup() {
 	var url = "addPhoto";
 	var name = "사진 첨부하기";
-
-	var option = "width = 500, height = 500, toolbar=no,location=no, status=no, scrollbars=no"
+	var option = "width = 500, height = 500, toolbar=no,location=no, status=no, scrollbars=no";
 		
+	window.open(url, name, option);
+}
+
+function popupForCancel(reserveSeq) {
+	var url = "cancelReserve/"+reserveSeq;
+	var name = "예약 취소 안내";
+	var option = "width = 500, height = 500, toolbar=no,location=no, status=no, scrollbars=no";
+
 	window.open(url, name, option);
 }
