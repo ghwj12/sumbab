e7515557c2aa78f71277f11ce3b9e5d04bb82971
JudithKim115 <82377269+JudithKim115@users.noreply.github.com>
2021-05-04 01:21:40 +0900

    Update ReviewController.java
    리뷰 작성할 때
    
    리뷰내용--->reviewService가 review table로 전송
    입력한 태그--->tagService가 tag table, store_tag, review_tag로 모두 전송

--- projectworkspace/sumbab/src/main/java/store/review/ReviewController.java
+++ projectworkspace/sumbab/src/main/java/store/review/ReviewController.java
@@ -1,21 +1,75 @@
 package store.review;
 
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import store.Store;
+import store.StoreService;
+import store.review.tag.TagService;
+
 @Controller
 public class ReviewController {
-
-	@RequestMapping(value="/review/writeReview", method=RequestMethod.GET)
-	public String writeReview() {
+	
+	@Autowired
+	private StoreService storeService;
+	@Autowired
+	public void setStoreService(StoreService storeService) {
+		this.storeService = storeService;
+	}
+	
+	@RequestMapping(value="/review/writeReview/{storeNum}", method=RequestMethod.GET)
+	public String writeReview(@PathVariable int storeNum, Model model, @ModelAttribute("storeVO")Store store) {
+		
+		model.addAttribute("reviewDTO", new Review());
+		model.addAttribute("storeVO", storeService.storeView(storeNum));//selectOne
 		
 		return "/review/writeReview";
 	}
 	
+	//사진첨부 popup url
 	@RequestMapping(value="/review/addPhoto", method=RequestMethod.GET)
 	public String popupForAddPhoto() {
 		
 		return "/review/addPhoto";
 	}
+	
+	@Autowired
+	private ReviewService reviewService;
+	
+	@Autowired
+	public void setReviewService(ReviewService reviewService) {
+		this.reviewService = reviewService;
+	}
+	@Autowired
+	private TagService tagService;
+	@Autowired
+	public void setTagService(TagService tagService) {
+		this.tagService = tagService;
+	}
+	
+	@RequestMapping(value="/review/completeWrReview", method=RequestMethod.POST)
+	public String completeWriteReview(@ModelAttribute("reviewDTO")Review review, ReviewRegitReq reqR,
+			@ModelAttribute("tag")ArrayList<String> tag, Model model, 
+			@ModelAttribute("storeNum")int storeNum, HttpSession session) {
+		
+		reviewService.regit(reqR, session, storeNum);
+		
+		for (int i = 0; i < tag.size(); i++) {
+			tagService.regist(tag.get(i));
+			tagService.insertReview_Tag();
+			tagService.insertStore_Tag(storeNum);
+		}
+		
+		return "/review/completeWrReview";
+	}
+	
 }
