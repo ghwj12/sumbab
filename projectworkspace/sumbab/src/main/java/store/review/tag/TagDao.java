c5658c086bc523489a5e2ef9f20d8f5f83f5964c
JudithKim115 <82377269+JudithKim115@users.noreply.github.com>
2021-05-04 01:20:18 +0900

    Update TagDao.java

--- projectworkspace/sumbab/src/main/java/store/review/tag/TagDao.java
+++ projectworkspace/sumbab/src/main/java/store/review/tag/TagDao.java
@@ -3,12 +3,12 @@ package store.review.tag;
 import java.util.List;
 
 public interface TagDao {
-
-	public void insert(Tag tag);
 	
+	public void insert(Tag tag);
 	//같은 태그 입력했을 때 count 올리는 update
-	public void update(String tagId);
+	public void update(String tagName);
 	public void delete(int reviewNum);
+	public int check(String tagName);
 	//작성된 태그는 수정(update)이 없다.
 	//리뷰에 작성된 태그 수정--> 태그를 삭제 하거나 새로운 태그를 추가하는 개념이기 때문.
 	
