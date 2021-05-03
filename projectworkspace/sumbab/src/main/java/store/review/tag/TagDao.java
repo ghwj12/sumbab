a21424b9555f52a3837203334836052e2a1a0a2b
JudithKim115 <82377269+JudithKim115@users.noreply.github.com>
2021-05-03 17:30:07 +0900

    Create TagDao.java

---
+++ projectworkspace/sumbab/src/main/java/store/review/tag/TagDao.java
@@ -0,0 +1,22 @@
+package store.review.tag;
+
+import java.util.List;
+
+public interface TagDao {
+
+	public void insert(Tag tag);
+	
+	//같은 태그 입력했을 때 count 올리는 update
+	public void update(String tagId);
+	public void delete(int reviewNum);
+	//작성된 태그는 수정(update)이 없다.
+	//리뷰에 작성된 태그 수정--> 태그를 삭제 하거나 새로운 태그를 추가하는 개념이기 때문.
+	
+	public List<Tag> selectTagByStore(int storeNum);
+	public List<Tag> selectTagByReview(int reviewNum);
+	
+	//중간테이블 insert Dao
+	public void insertReview_Tag();
+	public void insertStore_Tag(int storeNum);
+	
+}
