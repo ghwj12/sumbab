9a2ee6926edbe70283b78a3fd16a387285ac4cf2
JudithKim115 <82377269+JudithKim115@users.noreply.github.com>
2021-05-05 02:48:44 +0900

    Update SearchDao.java

--- projectworkspace/sumbab/src/main/java/store/search/SearchDao.java
+++ projectworkspace/sumbab/src/main/java/store/search/SearchDao.java
@@ -6,9 +6,7 @@ import store.Store;
 
 public interface SearchDao {
 
-	public List<Store> search(String input);
-	
-	//public List<Store> selectByKeyword(String keyword); //일반 검색
-	//public List<Store> selectByTag(String tag);  //해시태그 검색
+	public List<Store> selectByKeyword(String input); //일반 검색
+	public List<Store> selectByTag(String inputT);  //해시태그 검색
 	
 }
