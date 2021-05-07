ab28fd3d1ff294136cf22a557a0877d848247454
JudithKim115 <82377269+JudithKim115@users.noreply.github.com>
2021-05-08 05:04:35 +0900

    sql 대문자 C 변경, cotroller 이동

--- projectworkspace/sumbab/src/main/java/com/sumbab/sumbab/controller/SearchController.java
+++ projectworkspace/sumbab/src/main/java/com/sumbab/sumbab/controller/SearchController.java
@@ -1,4 +1,4 @@
-package com.sumbab.sumbab.model.search;
+package com.sumbab.sumbab.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.sumbab.sumbab.model.search.SearchService;
 
 @Controller
 public class SearchController {
