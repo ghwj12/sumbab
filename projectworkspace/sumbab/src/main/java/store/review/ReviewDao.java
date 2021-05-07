83a6eee721e517e28b03a9a824ed4fcbcfb5d269
JudithKim115 <82377269+JudithKim115@users.noreply.github.com>
2021-05-07 16:18:16 +0900

    Update ReviewDao.java

--- projectworkspace/sumbab/src/main/java/store/review/ReviewDao.java
+++ projectworkspace/sumbab/src/main/java/store/review/ReviewDao.java
@@ -2,6 +2,8 @@ package store.review;
 
 import java.util.List;
 
+import store.Store;
+
 public interface ReviewDao {
 
 	public void insert(Review review);
@@ -11,6 +13,9 @@ public interface ReviewDao {
 	public int getReviewCount(int storeNum);
 	public float getAvgStar(int storeNum);
 	public List<Review> getReviewList(int storeNum);
+	public List<Review> getMyReviewList(String id);
+	public List<Store> getStoreName(String id);
 	public float getAvgRevisit(int storeNum);
+	public Review reviewDetail(int reviewNum);
 	
 }
